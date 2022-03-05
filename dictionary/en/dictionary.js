13226a3cb648bc389fb4c0c0d3d5f442744f215f
Karl Jones <karljones@live.ie>
2022-03-05 12:16:50 +0000

    Added en dict

---
+++ dictionary/en/dictionary.js
@@ -0,0 +1,42 @@
+/**
+ * @author Karl Jones
+ * @email karljones@live.ie
+ * @create date 2022-03-05 11:23:56
+ * @modify date 2022-03-05 11:38:57
+ * @desc Hold information for number dictionary. https://en.wikipedia.org/wiki/Names_of_large_numbers
+ */
+export const Dictionary = {
+    units: [
+        "Un",
+        "Duo",
+        "Tre",
+        "Quattuor",
+        "Quinqua",
+        "Se",
+        "Septe",
+        "Octo",
+        "Nove"
+    ],
+    tens: [
+        "Deci",
+        "Viginti",
+        "Triginta",
+        "Quadraginta",
+        "Quinquaginta",
+        "Sexaginta",
+        "Septuaginta",
+        "Octoginta",
+        "Nonaginta"
+    ], 
+    hundreds: [
+        "Centi",
+        "Ducenti",
+        "Trecenti",
+        "Quadringenti",
+        "Quingenti",
+        "Sescenti",
+        "Septingenti",
+        "Octingenti",
+        "Nongenti"
+    ]
+}
