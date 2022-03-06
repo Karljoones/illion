7ba2ce262995905c2e2b11b9ae4e9ddad9137110
Karl Jones <karljones@live.ie>
2022-03-06 20:07:46 +0000

    Modifications to dictionary

--- dictionary/en/dictionary.js
+++ dictionary/en/dictionary.js
@@ -2,41 +2,21 @@
  * @author Karl Jones
  * @email karljones@live.ie
  * @create date 2022-03-05 11:23:56
- * @modify date 2022-03-05 11:38:57
+ * @modify date 2022-03-06 18:32:49
  * @desc Hold information for number dictionary. https://en.wikipedia.org/wiki/Names_of_large_numbers
  */
 export const Dictionary = {
-    units: [
-        "Un",
-        "Duo",
-        "Tre",
-        "Quattuor",
-        "Quinqua",
-        "Se",
-        "Septe",
-        "Octo",
-        "Nove"
-    ],
-    tens: [
-        "Deci",
-        "Viginti",
-        "Triginta",
-        "Quadraginta",
-        "Quinquaginta",
-        "Sexaginta",
-        "Septuaginta",
-        "Octoginta",
-        "Nonaginta"
-    ], 
-    hundreds: [
-        "Centi",
-        "Ducenti",
-        "Trecenti",
-        "Quadringenti",
-        "Quingenti",
-        "Sescenti",
-        "Septingenti",
-        "Octingenti",
-        "Nongenti"
-    ]
+    10e6: "Million",                // 1,000,000
+    10e9: "Billion",                // 1,000,000,000
+    10e12: "Trillion",              // 1,000,000,000,000
+    10e15: "Quadrillian",           // 1,000,000,000,000,000
+    10e18: "Quintillion",           // 1,000,000,000,000,000,000
+    10e21: "Sextillion",            // 1,000,000,000,000,000,000,000
+    10e24: "Septillion",            // 1,000,000,000,000,000,000,000,000
+    10e27: "Octillion",             // 1,000,000,000,000,000,000,000,000,000
+    10e30: "Nonillion",             // 1,000,000,000,000,000,000,000,000,000,000
+    10e33: "Decillion",             // 1,000,000,000,000,000,000,000,000,000,000,000
+    10e36: "Undecillion",           // 1,000,000,000,000,000,000,000,000,000,000,000,000
+    10e39: "Duodecillion",          // 1,000,000,000,000,000,000,000,000,000,000,000,000,000
+    10e42: "Tredecillion",          // 1,000,000,000,000,000,000,000,000,000,000,000,000,000,000
 }
